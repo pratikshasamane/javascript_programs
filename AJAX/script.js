b81a59b83f7e3e423a16878fb27accfaaa894da0
pratikshabs-maveric <pratikshab@maveric-systems.com>
2023-07-22 00:41:44 +0530

    Search field added

--- AJAX/script.js
+++ AJAX/script.js
@@ -2,111 +2,11 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
-
-/* Simple XMLHTTPRequest */
-
-/*
-
-const getCountries = function (country) {
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-  console.log(request.response);
-
-  request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `
-      <article class="country">
-              <img class="country__img" src="${data.flags.png}" />
-              <div class="country__data">
-                <h3 class="country__name">${data.name.common}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(
-                  +data.population / 1000000
-                ).toFixed(1)} people</p>
-                <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
-
-              </div>
-            </article>
-      `;
-
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-  });
-};
-
-getCountries("india");
-getCountries("canada");
-
-*/
-
-//******************************************************************************************************* */
-/* This approch is use if we dont want to change the sequence of country after refresh. this approach called as Callback Hell */
-
-// const getCountries = function (country) {
-//   // AJAX 1
-//   const request = new XMLHttpRequest();
-//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-//   request.send();
-//   console.log(request.response);
-
-//   request.addEventListener("load", function () {
-//     // data 1
-//     const [data] = JSON.parse(this.responseText);
-//     console.log(data);
-
-//     //render country
-//     renderCountries(data);
-
-//     // neighbour country
-//     const [neighbour] = data.borders;
-
-//     if (!neighbour) return;
-
-//     // AJAX 2
-//     const request2 = new XMLHttpRequest();
-//     request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
-//     request2.send();
-
-//     request2.addEventListener("load", function () {
-//       console.log(this.responseText);
-//       // data 2 (neighbour country)
-//       const [data2] = JSON.parse(this.responseText);
-
-//       // render neighbour country
-//       renderCountries(data2, "neighbour");
-//     });
-//   });
-// };
-
-// function renderCountries(data, className = "") {
-//   const html = `
-//         <article class="country ${className}">
-//                 <img class="country__img" src="${data.flags.png}" />
-//                 <div class="country__data">
-//                   <h3 class="country__name">${data.name.common}</h3>
-//                   <h4 class="country__region">${data.region}</h4>
-//                   <p class="country__row"><span>👫</span>${(
-//                     +data.population / 1000000
-//                   ).toFixed(1)} people</p>
-//                   <p class="country__row"><span>🗣️</span>${
-//                     data.languages.eng
-//                   }</p>
-
-//                 </div>
-//               </article>
-//         `;
-//   countriesContainer.insertAdjacentHTML("beforeend", html);
-// }
-// getCountries("india");
-
-//****************************************************************************************************************
-
-// Promises (best alternative for callback hell)
-
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
+  countriesContainer.innerHTML = "";
+  if (!`https://restcountries.com/v3.1/name/${country}`)
+    throw new Error(`The country you entered is wrong!`);
+  return fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then((response) => response.json())
     .then((data) => {
       renderCountries(data[0]);
@@ -114,11 +14,22 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
     })
     .then((response) => response.json())
-    .then((data) => renderCountries(data[0], "neighbour"))
-    .catch((error) => console.error(error.message));
+    .then((data) => {
+      return renderCountries(data[0], "neighbour");
+    })
+    .catch((error) => {
+      renderAlert(`Country is not availabele`);
+      console.error(error.message);
+    });
 };
 
-getCountryData("india");
+function renderAlert(msg) {
+  const html = `<div class="alert">
+ ${msg}
+</div>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+}
 
 function renderCountries(data, className = "") {
   const html = `
@@ -139,10 +50,3 @@ function renderCountries(data, className = "") {
         `;
   countriesContainer.insertAdjacentHTML("beforeend", html);
 }
-
-/*
-daily 2 program practice - interview
-AJAX- watch 4 videos udemy 
-interview questions - oops,core java, mysql, js ( daily one subject)
-Node js - start learning.
-*/
